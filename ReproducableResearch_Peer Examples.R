3daf71e66d333ab26fce8ad69f31f312778f30b8
behonji <billhoenig@gmail.com>
2015-10-04 11:43:08 -0600

    reproducable research peer review 2 examples that I like

---
+++ ReproducableResearch_Peer Examples.R
@@ -0,0 +1,10 @@
+
+# Student 2:
+# I like how he used both the x and y axis for Injuries and Fatalities
+# http://rpubs.com/avroethig/CRRPA2
+
+# Student 3:
+# This person provided the best graphs
+# He also did a good job at converting the signs relate to damage amount
+# http://rpubs.com/DK-DataScience/ReproducibleResearchAssessment2
+
